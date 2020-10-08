682e3b4fd114f370752316f6b5c79704da0757ef
rapixar <cudraphael@gmail.com>
2020-10-08 17:12:27 +0100

    git init

---
+++ Project/restAPI/APIProjectFolder/Prediction/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .apps import PredictionConfig
+import pandas as pd
+
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def api_add(request):
+    sum = 0
+    response_dict = {}
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        data = request.data
+        for key in data:
+            sum += data[key]
+        response_dict = {"sum": sum}
+    return Response(response_dict, status=status.HTTP_201_CREATED)
+
+
+# Class based view to predict based on IRIS model
+class IRIS_Model_Predict(APIView):
+    #permission_classes = [IsAuthenticated]
+    def post(self, request, format=None):
+        data = request.data
+        keys = []
+        values = []
+        for key in data:
+            keys.append(key)
+            values.append(data[key])
+        X = pd.Series(values).to_numpy().reshape(1, -1)
+        loaded_classifier = PredictionConfig.classifier
+        y_pred = loaded_classifier.predict(X)
+        y_pred = pd.Series(y_pred)
+        target_map = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
+        y_pred = y_pred.map(target_map).to_numpy()
+        response_dict = {"Prediced Iris Species": y_pred[0]}
+        return Response(response_dict, status=200)
